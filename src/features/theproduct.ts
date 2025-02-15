2e9c068288593530afdbd51f9dd2cfae1b85854d
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-02-16 01:49:03 +0200

    add logger and fetching products

--- src/features/theproduct.ts
+++ src/features/theproduct.ts
@@ -1,13 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+}
+
+interface ProductState {
+    items: Product[];
+    status: "idle" |"loading" | "succeeded"| "failed";
+}
+
+const initialState: ProductState = {
     items: [],
-    status: null
+    status: "idle",
 };
-const theproductSlice = createSlice({
-    name: "theproduct",
-    initialState,
-    reducers: {}
-    });
 
-export default theproductSlice.reducer;
+export const fetchedata = createAsyncThunk<Product[]>(
+    "product/fetchedata",
+    async () => {
+        const response = await axios.get("http://localhost:4000/product"); return response.data;
+    }
+);
+
+const productSlice = createSlice({
+    name: "product",
+    initialState ,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder .addCase(fetchedata.pending, (state) => {
+                state.status = "loading";
+            })
+            .addCase(fetchedata.fulfilled, (state, action: PayloadAction<Product[]>) => {
+                state.items = action.payload;
+                 state.status = "succeeded";
+            })
+            .addCase(fetchedata.rejected, (state) => {
+                state.status = "failed";
+            });
+    },
+});
+
+export default productSlice.reducer;
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import theproduct from "./features/theproduct";
-
+import { fetchedata } from "./features/theproduct";
 const store = configureStore({
   reducer: {
     theproduct: theproduct,
   },
 });
+
+store.dispatch(fetchedata());
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
