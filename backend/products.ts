4b2794d357d0960ada48be957f1fa6055d3e308d
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-02-15 18:58:05 +0200

    add product images use cloudinary

--- backend/products.ts
+++ backend/products.ts
@@ -3,19 +3,31 @@ const products = [
         id: 1,
         name: 'Product 1',
         price: 100,
-        image:""
+        image:"https://res.cloudinary.com/dgjbaeyok/image/upload/v1739638280/reactshoping/nizcva5bs3gemmc1iznd.jpg"
     },
     {
         id: 2,
         name: 'Product 2',
         price: 200,
-        image:""
+        image:"https://res.cloudinary.com/dgjbaeyok/image/upload/v1739638280/reactshoping/iwlykztdygajl6pz411g.webp"
     },
     {
         id: 3,
         name: 'Product 3',
         price: 300,
-        image:""
+        image:"https://res.cloudinary.com/dgjbaeyok/image/upload/v1739638279/reactshoping/zamor8beklbrfg6z4xrf.webp"
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        price: 300,
+        image:"https://res.cloudinary.com/dgjbaeyok/image/upload/v1739638279/reactshoping/ilyhkbtk8gdh8zcmnkgw.webp"
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        price: 400,
+        image:"https://res.cloudinary.com/dgjbaeyok/image/upload/v1739638279/reactshoping/aegecbpo7ubef0almsz2.webp"
     },
     ];
 
