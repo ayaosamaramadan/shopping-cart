3abe519484660318bed4be2c36b9109b25d5f38a
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-02-22 03:06:53 +0200

    setup Mongo connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 // const compression = require('compression');
 const products = require('./products');
 
@@ -16,8 +17,17 @@ app.get('/product', (req, res) => {
   res.send(products);
 });
 
+const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message);
+  });
